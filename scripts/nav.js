eef655dd82d031934208f6e146225c137055796e
John Charlton <coolmule0@hotmail.co.uk>
2017-02-27 09:51:33 +0000

    Still Basic layout
    Correct display of all pages, in a basic, incomplete form

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,8 +1,28 @@
-function myFunction() {
+function navFunction() {
 	var x= document.getElementById("myTopnav");
 	if (x.className === "topnav") {
 		x.className += " responsive";
 	} else {
 		x.className = "topnav";
 	}
+}
+
+function getMonth() {
+	var month = new Array();
+    month[0] = "January";
+    month[1] = "February";
+    month[2] = "March";
+    month[3] = "April";
+    month[4] = "May";
+    month[5] = "June";
+    month[6] = "July";
+    month[7] = "August";
+    month[8] = "September";
+    month[9] = "October";
+    month[10] = "November";
+    month[11] = "December";
+	
+	var d = new Date();
+    var n = month[d.getMonth()];
+	document.getElementById("month").innerHTML = n;
 }
